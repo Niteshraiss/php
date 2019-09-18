7226ae69b3bb3bcfc5caf2bd147e70823ca2272a
Nitesh <niteshrai182@gmail.com>
2019-09-18 18:10:37 +0530

    Update insert_try.php

--- insert_try.php
+++ insert_try.php
@@ -7,9 +7,9 @@ $con=new mysqli($na,$us,$pass,$db);
 if($con==TRUE)
 echo "Connected";
 //for inserting data
-    $quot=$_POST['quote']);
-    $da=$_POST['date']);
-    $name=$_POST['author']);
+    $quot=$_POST['quote'];
+    $da=$_POST['date'];
+    $name=$_POST['author'];
     $qu="insert into mytable(quote,date,author)values('$quot','$da','$name')";
     $qu=mysqli_query($con,$qu);
     if($qu==TRUE)
